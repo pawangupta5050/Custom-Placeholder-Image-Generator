5f9b0af253a7e6962dda092306c5fbf95dba227d
pawangupta5050 <85182279+pawangupta5050@users.noreply.github.com>
2023-08-17 16:07:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const inputAll = document.querySelectorAll('input')
+const selectDimension = document.querySelector('#selectImageDim')
+let dimension = document.querySelector('#dimension')
+// console.log(dimension)
+const textArea = document.querySelector('textarea')
+const image = document.querySelector('img')
+
+const copyURL = document.querySelector('.fa-solid')
+
+const removeHashTag = (str) => {
+    return str.replace("#","")
+}
+
+const addPlus = (str) => {
+    return str.split(" ").join("+");
+}
+
+const createURL = () => {
+
+    const selectedDimension = selectDimension.value;
+    dimension.textContent = selectDimension.value;
+    const textOnImage = addPlus(inputAll[0].value);
+    const backgroundColor = removeHashTag(inputAll[1].value);
+    const textColor = removeHashTag(inputAll[2].value);
+
+    const URL = `https://via.placeholder.com/${selectedDimension}/${backgroundColor}/${textColor}?text=${textOnImage}`
+    textArea.value = URL
+    image.src = URL
+    console.log(image)
+    
+    copyURL.classList.remove('fa-check')
+    copyURL.classList.add('fa-clipboard')
+}
+
+inputAll.forEach((input) => {
+    input.addEventListener('change', createURL);
+})
+
+selectDimension.addEventListener('change', createURL)
+
+copyURL.addEventListener('click', () => {
+    if(textArea.value){
+        navigator.clipboard.writeText(textArea.value)
+        .then(() => {
+            console.log('Text copied to clipboard');
+        })
+        .catch(err => {
+            console.error('Error copying text: ', err);
+        });
+
+        copyURL.classList.remove('fa-clipboard')
+        copyURL.classList.add('fa-check')
+    }
+})
+
+
+
+
